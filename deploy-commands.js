01e8419851c8f0677b44ddadf0f6faa9ae8730a3
R3IC0P <luxorpol@gmail.com>
2021-09-27 20:53:57 +0200

    add subcommands

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,7 +10,7 @@ const commands = [
 	new SlashCommandBuilder().setName('user').setDescription('Pokazuje twój nick i ID.'),
     new SlashCommandBuilder().setName('image').setDescription('Losowe zdjęcie z wpisanego tematu!'),
     new SlashCommandBuilder().setName('widzisz_mnie').setDescription('Srebrny Brek - widzisz mnie?'),
-	/*new SlashCommandBuilder()
+	new SlashCommandBuilder()
 		.setName('info')
 		.setDescription('Informacje na temat użytkownika lub serwera.')
 		.addSubcommand(subcommand =>
@@ -27,7 +27,7 @@ const commands = [
 			subcommand
 				.setName('server')
 				.setDescription('Informacje o serwerze')
-		),*/
+		),
 ]
 	.map(command => command.toJSON());
 
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 // Require the necessary discord.js classes
 const { channelMention } = require('@discordjs/builders');
 const { Client, Intents, MessageEmbed } = require('discord.js');
+const { promisify } = require('util');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const channel = client.channels.cache.get('id');
+// Variables
+//const channel = client.channels.cache.get('id');
+const wait = require('util').promisify(setTimeout);
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
+// Interactions with commands using "/"
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
@@ -41,15 +45,51 @@ client.on('interactionCreate', async interaction => {
 		});
     }
 	else if (commandName === 'widzisz_mnie') {
-		await interaction.reply('Widzisz mnie?');
-		const widziszMnie = new MessageEmbed()
+		const widziszMnieEmbed = new MessageEmbed()
 			.setColor('#00fff0')
 			.setTitle('Srebrny Brek')
-			.setDescription('Legendarna postać polskiego internetu\n- Ma ponad 70 lat\n- Nazywany "BREK" lub "Srebrny Brek"\n- Pochodzi z Żyrardowa')
+			.setDescription('**Legendarna postać polskiego internetu**\n- Ma ponad 70 lat\n- Nazywany "BREK" lub "Srebrny Brek"\n- Pochodzi z Żyrardowa\n\n**Piękne słowa**\nWidzisz mnie?')
 			.setThumbnail('https://steamuserimages-a.akamaihd.net/ugc/1701780966243068769/953B5469D8306D6D7D064F01320D903DA319CE40/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false')
-			.addField('Nagranie', 'https://youtu.be/K8hiXbyO8PU?t=4', true)
-			.setTimestamp();
-		await interaction.channel.send({ embeds: [widziszMnie] });
+			.addField('Nagranie', 'https://youtu.be/K8hiXbyO8PU?t=4', true);
+		await interaction.reply({
+			embeds: [widziszMnieEmbed]
+		});
+	}
+	else if (commandName === 'info') {
+		if (interaction.options.getSubcommand() == 'user') {
+			const user = interaction.options.getUser('target');
+			if (user) {
+				const userTargetEmbed = new MessageEmbed()
+					.setColor('#00fff0')
+					.setTitle('Informacje o użytkowniku')
+					.setDescription(`**Nazwa użytkownika**\n${user.username}\n\n**ID**\n${user.id}`)
+					.setThumbnail(user.avatarURL());
+				await interaction.reply({
+					embeds: [userTargetEmbed]
+				});
+			}
+			else {
+				const userYouEmbed = new MessageEmbed()
+					.setColor('#00fff0')
+					.setTitle('Informacje o użytkowniku')
+					.setDescription(`**Twoja nazwa użytkownika**\n${interaction.user.username}\n\n**Twoje ID**\n${interaction.user.id}`)
+					.setThumbnail(interaction.user.avatarURL());
+				await interaction.reply({
+					embeds: [userYouEmbed],
+					ephemeral: true
+				});
+			}
+		}
+		else if (interaction.options.getSubcommand() === 'server') {
+			const serverEmbed = new MessageEmbed()
+				.setColor('#00fff0')
+				.setTitle('Informacje o serwerze')
+				.setDescription(`**Nazwa serwera**\n${interaction.guild.name}\n\n**Ilość użytkowników**\n${interaction.guild.memberCount}`)
+				.setThumbnail(interaction.guild.iconURL());
+			await interaction.reply({
+				embeds: [serverEmbed]
+			});
+		}
 	}
 });
 
